c5aa289513ef53340d6c7a4ba7550856536daa5c
matula.kristof@vereb.dc <matula.kristof@vereb.dc>
2019-09-20 12:11:42 +0200

    Add project files.

---
+++ mind1/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace mind1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Szia hogy hívnak?");
+            var nev = Console.ReadLine();
+            Console.WriteLine($"Oh milyen szép név az hogy {nev} ! <3");
+            Console.WriteLine("Szerecc programozni?");
+            var valasz = Console.ReadLine();
+            if (valasz.ToLower() == "igen")
+            {
+                Console.WriteLine("Akkor mi nagyon jóü barátok leszünk.");
+            }
+            else
+            {
+                Console.WriteLine(":(");
+                Console.WriteLine("Akkor b@zd meg!");
+            }
+            Console.ReadKey();
+        }
+    }
+}
